72fb8eee3aa5b80b49046e98472718a1e408e76e
B CHUCHEL ASHISH <85539405+ashishrockz@users.noreply.github.com>
2024-06-07 00:21:45 +0530

    the vercle time

--- index.js
+++ index.js
@@ -25,17 +25,16 @@ app.use('/api/project', verifyToken, project);
 app.use('/api/sprint', verifyToken, sprint);
 app.use('/api/issue', verifyToken, issue);
 
-// const __dirname1 =path.resolve();
-// if(process.env.NODE_ENV === "production"){
-//   app.use(express.static(path.join(__dirname1, "/client/build/")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname1, "client", "bulid" , "index.html"));
-//   });
-// }else{
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+} else {
   app.get('/', (req, res) => {
     res.send('Welcome to the API');
   });
-// }
+}
 
 // Catch-all route for undefined routes
 app.use((req, res) => {
